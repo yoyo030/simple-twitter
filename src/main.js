e59e1571fa23a48906c18380f0bd095d0452cc0d
yoyo030 <jennifer77christian@gmail.com>
2022-10-03 23:34:56 +0800

    【把style獨立出來】刪除在App.vue裡的styl , 建立scss資料夾, 並引用進main.js內

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+//新增scss路線
+import '@/assets/styles/main.scss'
+
 Vue.config.productionTip = false;
 
 new Vue({
