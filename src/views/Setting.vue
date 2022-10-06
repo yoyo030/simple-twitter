321456bb0d615b8fcbdf18d984fe7bd5a0bea083
miaCCC <ta828383@gmail.com>
2022-10-06 20:38:16 +0800

    setting頁面分切component，新增JS資料傳遞邏輯

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -7,93 +7,9 @@
 
       <div class="w100 centerSection">
         <h4 class="setting-title main-title">帳戶設定</h4>
-        <div class="form-container ">
-    <form 
-     class="main d-flex flex-column align-items-center">
-      
-      <div class="setting-content">
-        <div class="form-label-group mb-2">
-          <label for="account">帳號</label>
-          <input
-            v-model="account"
-            id="account"
-            name="account"
-            type="text"
-            class="form-control"
-            placeholder="請輸入帳號"
-            autocomplete="useraccount"
-            required
-            autofocus
-          />
+        <div class="form-container">
+          <SettingForm @after-submit-setting="afterSubmitSetting" />
         </div>
-        <div class="form-label-group mb-2">
-          <label for="name">名稱</label>
-          <input
-            v-model="name"
-            id="name"
-            name="name"
-            type="text"
-            class="form-control"
-            placeholder="請輸入名稱"
-            autocomplete="username"
-            required
-            autofocus
-          />
-        </div>
-
-        <div class="form-label-group mb-2">
-          <label for="email">Email</label>
-          <input
-            v-model="email"
-            id="email"
-            name="email"
-            type="email"
-            class="form-control"
-            placeholder="請輸入Email"
-            autocomplete="email"
-            required
-          />
-        </div>
-
-        <div class="form-label-group mb-3">
-          <label for="password">密碼</label>
-          <input
-            v-model="password"
-            id="password"
-            name="password"
-            type="password"
-            class="form-control"
-            placeholder="請輸入密碼"
-            autocomplete="new-password"
-            required
-          />
-        </div>
-
-        <div class="form-label-group mb-3">
-          <label for="password-check">密碼確認</label>
-          <input
-            v-model="passwordCheck"
-            id="password-check"
-            name="passwordCheck"
-            type="password"
-            class="form-control"
-            placeholder="請再次輸入密碼"
-            autocomplete="new-password"
-            required
-          />
-        </div>
-      </div>
-      </form>
-        <button 
-         class="setting-save-button" 
-         type="submit"
-         >
-         儲存
-        </button>
- 
-
-   
-  </div>
       </div>
 
       <div class="w100 rightSection">popularList</div>
@@ -105,8 +21,23 @@
 
 <script>
 import NavBar from "../components/NavBar.vue";
+import SettingForm from "../components/SettingForm.vue";
+
 export default {
-  name: "UserProfile",
-  components: { NavBar },
+  name: "Setting",
+  components: {
+    NavBar,
+    SettingForm,
+  },
+
+  methods: {
+    afterSubmitSetting(formData) {
+      console.log(formData);
+      // 透過 API 將表單資料送到伺服器
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ": " + value);
+      }
+    },
+  },
 };
 </script>
