5a69c5b03430da44f4b01af4cbe309bd97ced00c
miaCCC <ta828383@gmail.com>
2022-10-07 18:57:32 +0800

    main主頁共用樣式、排版、RWD更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,22 @@
 <template>
-<div class="d-flex justify-content-center w100">
-<div class="d-flex w100">
-<div class="w100 leftSection" ><NavBar /></div>
-<div class="w100 centerSection">
+
+  <div class="container d-flex justify-content-center w100">
+      <div class="w100 leftSection"><NavBar /></div>
+      <div class="w100 centerSection">
         <h4 class="setting-title main-title">首頁</h4>
       </div>
-<div class="w100 rightSection" >popularList</div>
-</div>
-</div>
+      <div class="w100 rightSection">popularList</div>
+    </div>
+
+
 </template>
 
 
-<style scoped lang="scss">
-@import "@/assets/styles/_Home.scss";
-</style>
-<script>
 
-import NavBar from '../components/NavBar.vue';
+<script>
+import NavBar from "../components/NavBar.vue";
 export default {
-    name: "UserProfile",
-    components: { NavBar }
-}
+  name: "UserProfile",
+  components: { NavBar },
+};
 </script>
