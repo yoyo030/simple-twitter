a235134f111093272f9d8109e7c7058d7a472304
yoyo030 <jennifer77christian@gmail.com>
2022-10-09 21:54:20 +0800

    TweetList 串api

--- src/components/TweetList.vue
+++ src/components/TweetList.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="home-tweet-container">
-    
+
+    <div class="home-tweet-box">
+      <div class="d-flex align-items-center">
+        <img
+          v-if="currentUser.avatar != undefined"
+          :src="currentUser.avatar"
+          class="tweet-list-tweet-img"
+          alt=""
+        />
+        <img
+          v-else
+          src="../assets/images/logo-gray.png"
+          class="tweet-list-tweet-img"
+          alt=""
+        />
+        <textarea
+          class="home-tweet-input"
+          type="text"
+          name="home-tweet-input"
+          placeholder="有什麼新鮮事？"
+        />
+      </div>
+      <button class="home-tweet-button">推文</button>
+    </div>
+
 
     <div class="tweet-list scrollbar">
       <div class="tweet d-flex" v-for="tweet in tweets" :key="tweet.id">
         <!--待串接後用v-bind改為使用者img-->
         <img
+          v-if="tweet.User.avatar != undefined"
+          :src="tweet.User.avatar"
+          class="tweet-list-tweet-img"
+          alt=""
+        />
+        <img
+          v-else
           src="../assets/images/logo-gray.png"
           class="tweet-list-tweet-img"
           alt=""
@@ -14,10 +45,10 @@
         <div class="tweet-list-text d-flex flex-column">
           <div class="tweet-list-tweet-top d-flex align-items-center">
             <div class="tweet-user-name">
-              {{ tweet.name }}
+              {{ tweet.User.name }}
             </div>
             <div class="tweet-user-account">
-              {{ tweet.account | addPrefix }}
+              {{ tweet.User.account | addPrefix }}
             </div>
             <span>・</span>
             <div class="tweet-user-createdAt">
@@ -29,7 +60,7 @@
             :to="{ name: 'reply-list', params: { id: tweet.id } }"
           >
             <div class="tweet-contentText">
-              {{ tweet.contentText }}
+              {{ tweet.description }}
             </div>
           </router-link>
           <div class="tweet-action d-flex">
@@ -39,9 +70,7 @@
                 class="icon cursor-pointer"
                 alt=""
               />
-              <div class="tweet-reply-amount number-font">
-                {{ tweet.reply.length }}
-              </div>
+              <div class="tweet-reply-amount number-font">推特回應數</div>
             </div>
             <div class="tweet-like d-flex">
               <img
@@ -71,19 +100,15 @@ a:hover {
 
 <script>
 import { fromNowFilter } from "./../utils/mixins";
-
+import { mapState } from "vuex";
+import authorizationAPI from "./../apis/authorization";
+import { Toast } from "./../utils/helpers";
 export default {
   name: "TweetList",
   mixins: [fromNowFilter],
-  props: {
-    initialTweets: {
-      type: Array,
-      required: true,
-    },
-  },
   data() {
     return {
-      tweets: this.initialTweets,
+      tweets: [],
     };
   },
   filters: {
@@ -94,6 +119,32 @@ export default {
         return `@${account}`;
       }
     },
+  },
+  methods: {
+    async fetchTweets() {
+      try {
+        const response = await authorizationAPI.getTweets();
+        const data = response.data;
+        //console.log(data);
+        if (data.status && data.status !== "success") {
+          throw new Error(data.message);
+        }
+        this.tweets = data;
+      } catch (error) {
+        console.log(error);
+          Toast.fire({
+          icon: "warning",
+          title: `資料載入失敗 !`,
+        });
+      }
+    },
+  },
+  created() {
+    this.fetchTweets();
+  },
+
+  computed: {
+    ...mapState(["currentUser", "isAuthenticated"]),
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,19 +4,7 @@
       <div class="w100 leftSection"><NavBar /></div>
       <div class="w100 centerSection">
         <h4 class="setting-title main-title">首頁</h4>
-         <div class="home-tweet-box">
-      <div class="d-flex align-items-center">
-        <img src="../assets/images/ac-logo.png" class="home-tweet-img" alt="" />
-        <textarea
-          class="home-tweet-input"
-          type="text"
-          name="home-tweet-input"
-          placeholder="有什麼新鮮事？"
-        />
-      </div>
-      <button class="home-tweet-button">推文</button>
-    </div>
-        <TweetList :initial-tweets="tweets"/>
+        <TweetList/>
       </div>
       <div class="w100 rightSection"><RecommandedList /></div>
     </div>
@@ -29,83 +17,7 @@ import NavBar from "../components/NavBar.vue";
 import RecommandedList from "../components/RecommandedList.vue";
 import TweetList from "../components/TweetList.vue";
 
-const dummyData = {
-  tweets: [  
-    {
-      id: 1,
-      account: "apple123",
-      name: "apple",
-      img: "../assets/images/logo-gray.png",
-      contentText: "滿員御禮!",
-      createdAt: "2022-10-08",
-      reply: [
-        "good", "good", "good"
-      ],
-      likeAmount: 520      
-    },
-    {
-      id: 1,
-      account: "apple123",
-      name: "apple",
-      img: "../assets/images/logo-gray.png",
-      contentText: "滿員御禮!",
-      createdAt: "2022-10-08",
-      reply: [
-        "good", "good", "good"
-      ],
-      likeAmount: 520     
-    },
-    {
-      id: 1,
-      account: "apple123",
-      name: "apple",
-      img: "../assets/images/logo-gray.png",
-      contentText: "滿員御禮!",
-      createdAt: "2022-10-08",
-      reply: [
-        "good", "good", "good"
-      ],
-      likeAmount: 520      
-    },  
-    {
-      id: 1,
-      account: "apple123",
-      name: "apple",
-      img: "../assets/images/logo-gray.png",
-      contentText: "滿員御禮!",
-      createdAt: "2022-10-08",
-      reply: [
-        "good", "good", "good"
-      ],
-      likeAmount: 520  
-    },
-    {
-      id: 1,
-      account: "apple123",
-      name: "apple",
-      img: "../assets/images/logo-gray.png",
-      contentText: "滿員御禮!",
-      createdAt: "2022-10-08",
-      reply: [
-        "good", "good", "good"
-      ],
-      likeAmount: 520      
-    },
-    {
-      id: 1,
-      account: "apple123",
-      name: "apple",
-      img: "../assets/images/logo-gray.png",
-      contentText: "滿員御禮!",
-      createdAt: "2022-10-08",
-      reply: [
-        "good", "good", "good"
-      ],
-      likeAmount: 520      
-    },
 
-  ]
-}
 
 export default {
   name: "Home",
@@ -113,37 +25,6 @@ export default {
     NavBar,
     RecommandedList,
     TweetList
-  },
-
-  data() {
-    return {
-      tweets: [],
-    }
-  },
-  created() {
-    this.fetchTweets()
-  },
-  methods: {
-    fetchTweets() {
-        this.tweets = dummyData.tweets
-      }
-    }
- 
+  }, 
 };
-
-//串接API用程式碼(async/await語法)
-//async fetchTweets() {
-//      try {
-//        this.isLoading = true
-//        const { data } = await tweetAPI.getAll()
-//        if (data.status === 'error') {
-//          throw new Error(data.message)
-//        }
-//        this.tweets = data
-//        this.isLoading = false
-//      } catch (error) {
-//        this.isLoading = false
-//        console.error
-//      }
-//    }
 </script>
