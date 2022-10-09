03c21d4d6d8bc92e3cdb507e73c840235165aeca
yoyo030 <jennifer77christian@gmail.com>
2022-10-09 21:47:10 +0800

    串setting頁面,TweetList元件 api

--- src/components/TweetList.vue
+++ src/components/TweetList.vue
@@ -2,7 +2,18 @@
   <div class="home-tweet-container">
     <div class="home-tweet-box">
       <div class="d-flex align-items-center">
-        <img src="../assets/images/ac-logo.png" class="home-tweet-img" alt="" />
+        <img
+          v-if="currentUser.avatar != undefined"
+          :src="currentUser.avatar"
+          class="tweet-list-tweet-img"
+          alt=""
+        />
+        <img
+          v-else
+          src="../assets/images/logo-gray.png"
+          class="tweet-list-tweet-img"
+          alt=""
+        />
         <textarea
           class="home-tweet-input"
           type="text"
@@ -17,6 +28,13 @@
       <div class="tweet d-flex" v-for="tweet in tweets" :key="tweet.id">
         <!--待串接後用v-bind改為使用者img-->
         <img
+          v-if="tweet.User.avatar != undefined"
+          :src="tweet.User.avatar"
+          class="tweet-list-tweet-img"
+          alt=""
+        />
+        <img
+          v-else
           src="../assets/images/logo-gray.png"
           class="tweet-list-tweet-img"
           alt=""
@@ -25,10 +43,10 @@
         <div class="tweet-list-text d-flex flex-column">
           <div class="tweet-list-tweet-top d-flex align-items-center">
             <div class="tweet-user-name">
-              {{ tweet.name }}
+              {{ tweet.User.name }}
             </div>
             <div class="tweet-user-account">
-              {{ tweet.account | addPrefix }}
+              {{ tweet.User.account | addPrefix }}
             </div>
             <span>・</span>
             <div class="tweet-user-createdAt">
@@ -40,7 +58,7 @@
             :to="{ name: 'reply-list', params: { id: tweet.id } }"
           >
             <div class="tweet-contentText">
-              {{ tweet.contentText }}
+              {{ tweet.description }}
             </div>
           </router-link>
           <div class="tweet-action d-flex">
@@ -50,9 +68,7 @@
                 class="icon cursor-pointer"
                 alt=""
               />
-              <div class="tweet-reply-amount number-font">
-                {{ tweet.reply.length }}
-              </div>
+              <div class="tweet-reply-amount number-font">推特回應數</div>
             </div>
             <div class="tweet-like d-flex">
               <img
@@ -73,19 +89,15 @@
 
 <script>
 import { fromNowFilter } from "./../utils/mixins";
-
+import { mapState } from "vuex";
+import authorizationAPI from "./../apis/authorization";
+import { Toast } from "./../utils/helpers";
 export default {
   name: "TweetList",
   mixins: [fromNowFilter],
-  props: {
-    initialTweets: {
-      type: Array,
-      required: true,
-    },
-  },
   data() {
     return {
-      tweets: this.initialTweets,
+      tweets: [],
     };
   },
   filters: {
@@ -96,6 +108,32 @@ export default {
         return `@${account}`;
       }
     },
+  },
+  methods: {
+    async fetchTweets() {
+      try {
+        const response = await authorizationAPI.getTweets();
+        const data = response.data;
+        //console.log(data);
+        if (data.status && data.status !== "success") {
+          throw new Error(data.message);
+        }
+        this.tweets = data;
+      } catch (error) {
+        console.log(error);
+          Toast.fire({
+          icon: "warning",
+          title: `資料載入失敗 !`,
+        });
+      }
+    },
+  },
+  created() {
+    this.fetchTweets();
+  },
+
+  computed: {
+    ...mapState(["currentUser", "isAuthenticated"]),
   },
 };
 </script>
