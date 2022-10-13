b1e6a381d0c8929f5251efcb888cc09f0e7dc758
yoyo030 <jennifer77christian@gmail.com>
2022-10-13 20:11:17 +0800

    加上追蹤功能

--- src/components/FollowerList.vue
+++ src/components/FollowerList.vue
@@ -3,14 +3,14 @@
     <div v-if="!followers" class="d-flex justify-content-center">
       使用者還沒有任何追隨者
     </div>
-    <div class="reply d-flex" v-for="follower in followers" :key="follower.id">
+    <div  v-else class="reply d-flex" v-for="follower in followers" :key="follower.id">
       <!--待串接後用v-bind改為使用者img-->
 
       <img :src="follower.avatar | emptyImage" class="user-img" alt="" />
 
       <div class="reply-list-text d-flex flex-column">
         <div class="tweet-list-tweet-top d-flex align-items-center">
-          <router-link
+          <router-link v-if= follower.id
             :to="{ name: 'user-profile', params: { id: follower.id } }"
           >
             <div class="tweet-user-name">
@@ -20,14 +20,14 @@
           <button
             v-if="follower.isFollowed"
             class="btn-isFollowed"
-            @click.stop.prevent="unFollowed(follower.id)"
+            @click.stop.prevent="unFollowed(follower)"
           >
             正在跟隨
           </button>
           <button
             v-else
             class="btn-unFollow"
-            @click.stop.prevent="followed(follower.id)"
+            @click.stop.prevent="followed(follower)"
           >
             跟隨
           </button>
@@ -43,7 +43,8 @@
 
 <script>
 import { emptyImageFilter } from "./../utils/mixins";
-
+import authorizationAPI from "./../apis/authorization";
+import { Toast } from "./../utils/helpers";
 export default {
   name: "userFollower",
   mixins: [emptyImageFilter],
@@ -68,6 +69,52 @@ export default {
         this.followers = null;
       }
       this.followers = this.initialFollower;
+    },
+        async followed(user) {
+      try {
+        let followedResponse = await authorizationAPI.followed(user.id);
+        let followedData = followedResponse.data;
+        console.log(followedData);
+
+        if (followedData.status && followedData.status !== "success") {
+          throw new Error(followedData.message);
+        }
+        
+        user.isFollowing = true;
+        Toast.fire({
+          icon: "success",
+          title: "追蹤成功",
+        });
+
+      } catch (error) {
+        console.log(error);
+        Toast.fire({
+          icon: "error",
+          title: error.message,
+        });
+      }
+    },
+    async unFollowed(user) {
+      try {
+        let unfollowedResponse = await authorizationAPI.unfollowed(user.id);
+        let unfollowedData = unfollowedResponse.data;
+        console.log(unfollowedData);
+
+        if (unfollowedData.status && unfollowedData.status !== "success") {
+          throw new Error(unfollowedData.message);
+        }
+        user.isFollowing = false;
+        Toast.fire({
+          icon: "success",
+          title: "取消追蹤成功",
+        });
+      } catch (error) {
+        console.log(error);
+        Toast.fire({
+          icon: "error",
+          title: error.message,
+        });
+      }
     },
   },
 
